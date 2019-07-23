c5f4a13ccfb51d7fb3848eb90191d5772cd94985
RichardMasson <richardmassn@gmail.com>
2019-07-23 21:57:38 +0200

    Final touches. Added comments and made debouncing stricter. Prac finished.

--- bincount.py
+++ bincount.py
@@ -8,7 +8,7 @@ Student Number: <MSSRIC004>
 Prac: <1>
 Date: <23/06/19>
 """
-# define pins for later
+# define pin numbers for later
 led1 = 11
 led2 = 16
 led3 = 15
@@ -16,22 +16,22 @@ but1 = 12
 but2 = 7
 
 # counter tracker
-counter = 0
+counter = 0 #track the digital count that the LEDs must display
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
 import time
 import itertools
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led1, GPIO.OUT) #output for the LEDs
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
-GPIO.setup((but1,but2), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup((but1,but2), GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #input for the buttons
 
 def press_1(channel):
     #print('Decrement')
     global counter
-    if (counter == 0):
+    if (counter == 0): #rollunder protection
         counter = 7
     else:
         counter -= 1
@@ -39,31 +39,32 @@ def press_1(channel):
 def press_2(channel):
     #print('Increment')
     global counter
-    if (counter == 7):
+    if (counter == 7): #rollover protection
         counter = 0
     else:
         counter += 1
 
 def press_toggle(channel):
-    GPIO.output(led1, not GPIO.input(led1))
+    GPIO.output(led1, not GPIO.input(led1)) #interrupt handler
 
 # interrupt stuff
-GPIO.add_event_detect(but1, GPIO.RISING, callback=press_1, bouncetime=150)
-GPIO.add_event_detect(but2, GPIO.RISING, callback=press_2, bouncetime=150)
+GPIO.add_event_detect(but1, GPIO.RISING, callback=press_1, bouncetime=250) #dec
+GPIO.add_event_detect(but2, GPIO.RISING, callback=press_2, bouncetime=250) #inc
 
 # binary table creation
-table = itertools.product('01', repeat=3)
+table = itertools.product('01', repeat=3) #generate iterator with binary values
 arr = []
 for i in table:
-    arr.append(i)
+    arr.append(i) #convery this to an array of binary lists
+    # for easier element retrieval
 
 # Logic that you write
 def main():
-    readout = arr[counter]
-    in1 = int(readout[0])
-    in2 = int(readout[1])
-    in3 = int(readout[2])
-    GPIO.output(led1,in1)
+    readout = arr[counter] #get the current binary value array
+    in1 = int(readout[0]) #typecast the string in place 1 to int
+    in2 = int(readout[1]) #same here
+    in3 = int(readout[2]) #same here
+    GPIO.output(led1,in1) #LED state should match the int from respective element
     GPIO.output(led2,in2)
     GPIO.output(led3,in3)
     

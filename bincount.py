0a7b02db41b913ec9756b3aa3f68ca2305d9ac49
RichardMasson <richardmassn@gmail.com>
2019-07-23 15:37:21 +0200

    LED can flicker

--- bincount.py
+++ bincount.py
@@ -12,18 +12,27 @@ Date: <23/06/19>
 led1 = 11
 led2 = 16
 led3 = 15
+but1 = 12
+but2 = 7
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
+GPIO.setup(but1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# interrupt stuff
+GPIO.add_event_detect(but1, GPIO.RISING)
 
 # Logic that you write
 def main():
-    GPIO.output((led1, led2, led3), GPIO.HIGH)
-
+    GPIO.output(led1, 1)
+    time.sleep(1)
+    GPIO.output(led1, 0)
+    time.sleep(1)
 
 # Only run the functions if 
 if __name__ == "__main__":
@@ -36,4 +45,7 @@ if __name__ == "__main__":
         # Turn off your GPIOs here
         GPIO.cleanup()
     except:
+        GPIO.cleanup()
         print("Some other error occurred")
+
+

1b9d8c3ecc134cf7e6f491e3a2e5083784e0da69
RichardMasson <richardmassn@gmail.com>
2019-07-23 21:16:36 +0200

    Toggle switch completed

--- bincount.py
+++ bincount.py
@@ -15,6 +15,12 @@ led3 = 15
 but1 = 12
 but2 = 7
 
+# counter tracker
+counter = 0
+
+# binary table
+# list(itertools.product(*['1', 2))
+
 # import Relevant Librares
 import RPi.GPIO as GPIO
 import time
@@ -22,20 +28,34 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
-GPIO.setup(but1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup((but1,but2), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def press_callback(channel):
-    print('Success!')
+def press_1(channel):
+    print('Press 1')
+    global counter
+    if (counter == 7):
+        counter = 0
+    else:
+        counter += 1
+
+def press_2(channel):
+    print('Press 2')
+    global counter
+    if (counter == 0):
+        counter = 7
+    else:
+        counter -= 1
+
+def press_toggle(channel):
+    GPIO.output(led1, not GPIO.input(led1))
 
 # interrupt stuff
-GPIO.add_event_detect(but1, GPIO.RISING, callback=press_callback, bouncetime=150)
+GPIO.add_event_detect(but1, GPIO.RISING, callback=press_toggle, bouncetime=150)
+GPIO.add_event_detect(but2, GPIO.RISING, callback=press_2, bouncetime=150)
 
 # Logic that you write
 def main():
-    GPIO.output(led1, 1)
-    time.sleep(1)
-    GPIO.output(led1, 0)
-    time.sleep(1)
+    time.sleep(0.1)
     
 # Only run the functions if 
 if __name__ == "__main__":
